dccb9ac440c3be185d974ac24dc1096d77bc3548
ymiah1871-spec <ymiah1871@gmail.com>
2026-02-06 11:58:04 +0000

    Final game update

--- server/index.ts
+++ server/index.ts
@@ -1,41 +1,34 @@
-import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes";
-import { setupVite } from "./vite";
+import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
 const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: { origin: "*" }
-});
+const io = new Server(httpServer, { cors: { origin: "*" } });
 
-// মাল্টিপ্লেয়ার লজিক
+app.use(express.static("client"));
+
+let players: any = {};
+
 io.on("connection", (socket) => {
-  console.log(`একজন বন্ধু জয়েন করেছে: ${socket.id}`);
-  
-  socket.on("monsterHit", (data) => {
-    // একজনের হিট সবার কাছে পাঠিয়ে দেওয়া
-    io.emit("monsterDamaged", data);
-  });
+    players[socket.id] = { id: socket.id, x: 0, z: 0 };
+    io.emit("updateList", Object.values(players));
 
-  socket.on("disconnect", () => {
-    console.log("একজন বন্ধু ডিসকানেক্ট হয়েছে");
-  });
+    socket.on("move", (data) => {
+        if (players[socket.id]) {
+            players[socket.id].x = data.x;
+            players[socket.id].z = data.z;
+            socket.broadcast.emit("playerMoved", players[socket.id]);
+        }
+    });
+
+    socket.on("disconnect", () => {
+        delete players[socket.id];
+        io.emit("updateList", Object.values(players));
+    });
 });
 
-(async () => {
-  await registerRoutes(httpServer, app);
-
-  if (app.get("env") === "development") {
-    await setupVite(httpServer, app);
-  }
-
-  const PORT = 5000;
-  httpServer.listen(PORT, "0.0.0.0", () => {
-    console.log(`গেমটি পোর্ট ${PORT} এ চলছে`);
-  });
-})();
+const PORT = 5000;
+httpServer.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
